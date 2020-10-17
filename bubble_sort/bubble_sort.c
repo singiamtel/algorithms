f123a6edb07b00fc460b7e9c70d054552f829655
Sergio <sergiogarciacotos@gmail.com>
2020-10-17 20:15:27 +0200

    set constant variables to const

--- bubble_sort/bubble_sort.c
+++ bubble_sort/bubble_sort.c
@@ -2,14 +2,15 @@
 #include <stdlib.h>	// for malloc
 #include <stdint.h> // for uint_t
 #include <time.h>	// for clock()
+#include <assert.h>	// for assert
 
-static inline void swap(uint64_t *array, uint32_t i, uint32_t j){
+static inline void swap(uint64_t *array, const uint32_t i, const uint32_t j){
 	uint32_t tmp = array[i];
 	array[i] = array[j];
 	array[j] = tmp;
 }
 
-void bubble_sort(uint64_t * array, uint32_t size){
+void bubble_sort(uint64_t * array, const uint32_t size){
 	for(int i=0; i<size; ++i){
 		for(int j=0; j<size-i-1; ++j){
 			if(array[j] > array [j+1]) swap(array, j, j+1);
@@ -17,13 +18,13 @@ void bubble_sort(uint64_t * array, uint32_t size){
 	}
 }
 
-void test(uint64_t* array, uint32_t size){
+void test(uint64_t* array, const uint32_t size){
 	clock_t start, end;
 	start = clock();
 	bubble_sort(array, size);
 	end = clock();
-	for(int i=0; i<size; ++i){
-		printf("%ld\n", array[i]);
+	for(int i=0; i<size-1; ++i){
+		assert(array[i] <= array[i+1]);
 	}
 	printf("Sorting took %f seconds\n", ((double) (end - start)) / CLOCKS_PER_SEC);
 }
@@ -33,10 +34,10 @@ int main(int argc, char * argv[]){
 	uint32_t filesize = 50, size = 0;
 	uint64_t last;
 	uint64_t* file_array = (uint64_t*) malloc(sizeof(uint64_t) * filesize);
-	fscanf(input, "%ld", &last);
+	(void)fscanf(input, "%ld", &last);
 	while (!feof(input))
 	{
-		fscanf(input, "%ld", &last);
+		(void)fscanf(input, "%ld", &last);
 		if(size >= filesize){
 			filesize+=30;
 			file_array = realloc(file_array, sizeof(uint64_t) * (filesize));
